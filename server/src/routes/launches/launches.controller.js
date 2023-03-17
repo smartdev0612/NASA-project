f4732b7d7bf6c8b1ce219b433e21d3547cec0f65
Smart Dev <danieldev0612@gmail.com>
2023-03-16 18:54:51 -0700

    aborting launch

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.model')
+const {
+  existsLaunchWithId,
+  getAllLaunches,
+  addNewLaunch,
+  abortLaunchById,
+} = require('../../models/launches.model')
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches())
@@ -30,15 +35,18 @@ function httpAddNewLaunch(req, res) {
 }
 
 function httpAbortLaunch(req, res) {
-  const launchId = req.params.id
+  const launchId = Number(req.params.id)
 
-  // if launch doesn't exist
-  return res.status(404).json({
-    error: 'Launch not found',
-  })
+  if (!existsLaunchWithId(launchId)) {
+    // if launch doesn't exist
+    return res.status(404).json({
+      error: 'Launch not found',
+    })
+  }
 
   // if launch does exist
-  return res.status(200).json()
+  const aborted = abortLaunchById(launchId)
+  return res.status(200).json(aborted)
 }
 
 module.exports = {

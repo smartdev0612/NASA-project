0d7a5f110e2d9f04a4f5993e690d3dc7e0d17102
Smart Dev <danieldev0612@gmail.com>
2023-03-16 20:06:38 -0700

    create script to test POST request

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,12 +1,42 @@
+const request = require('supertest')
+const app = require('../../app')
+
 describe('Test GET /launches', () => {
-  test('it should respond with 200 success', () => {
-    const response = 200
-    expect(response).toBe(200)
+  test('it should respond with 200 success', async () => {
+    const response = await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200)
   })
 })
 
 describe('TEST POST /launches', () => {
-  test('it should respond with 200 success', () => {})
+  const completeLaunchData = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186',
+    launchDate: 'January 4, 2028',
+  }
+
+  const launchDataWithoutDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-186',
+  }
+
+  test('it should respond with 201 created', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(completeLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201)
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf()
+    const responseDate = new Date(response.body.launchDate).valueOf()
+    expect(responseDate).toBe(requestDate)
+
+    expect(response.body).toMatchObject(launchDataWithoutDate)
+  })
 
   test('it should catch missing required properties', () => {})
 

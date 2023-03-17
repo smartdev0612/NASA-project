c8e18cb8251e823ab44074280658b6dd258b0dfe
Smart Dev <danieldev0612@gmail.com>
2023-03-16 19:42:21 -0700

    install jest, create api testing cases

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe('Test GET /launches', () => {
+  test('it should respond with 200 success', () => {
+    const response = 200
+    expect(response).toBe(200)
+  })
+})
+
+describe('TEST POST /launches', () => {
+  test('it should respond with 200 success', () => {})
+
+  test('it should catch missing required properties', () => {})
+
+  test('it should catch invalid dates', () => {})
+})

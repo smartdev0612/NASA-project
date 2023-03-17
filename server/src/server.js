862c9d56a4c305de823d4da348943e0d6c553bc6
Smart Dev <danieldev0612@gmail.com>
2023-03-17 16:15:35 -0700

    create mongo url

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,8 @@ const { loadPlanetsData } = require('./models/planets.model')
 
 const PORT = process.env.PORT || 8000
 
+const MONGO_URL = `mongodb+srv://daniel:OmruuYsrb3Zu50ko@nasacluster.4jetn0y.mongodb.net/nasa?retryWrites=true&w=majority`
+
 const server = http.createServer(app)
 
 async function startServer() {

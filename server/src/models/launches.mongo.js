2a16963b50d733c9e93a4f74d0dcc4891ada035c
Smart Dev <danieldev0612@gmail.com>
2023-03-17 21:21:15 -0700

    upsert operation

--- server/src/models/launches.mongo.js
+++ server/src/models/launches.mongo.js
@@ -35,3 +35,6 @@ const launchesSchema = new mongoose.Schema({
     default: true,
   },
 })
+
+// Connects launchesSchema with the "launches" collection
+module.exports = mongoose.model('Launch', launchesSchema)
